a2c47b5165203a02e56f6dcbc635b213f5dd1020
leandro8080 <leandro.schultze@ict.csbe.ch>
2025-03-06 14:21:46 +0100

    Show online users

--- client/script.js
+++ client/script.js
@@ -21,6 +21,12 @@
         break;
       case 'activeUsers':
         activeUsers = message.users;
+        const activeUsersList = document.getElementById('activeUsers');
+        activeUsersList.innerHTML = '';
+        activeUsers.forEach((user) => {
+          console.log(user);
+          activeUsersList.innerHTML += `<p>${user.name}</p>`;
+        });
         break;
       case 'typing':
         typingUsers = message.users;
